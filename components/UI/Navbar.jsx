2d187f093d482015cb88d376fd1bb05f4df87389
TeeJayGTAN <oniretemisan@gmail.com>
2023-11-25 13:25:29 +0100

    png fix

--- components/UI/Navbar.jsx
+++ components/UI/Navbar.jsx
@@ -11,7 +11,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { db } from "../../firebase";
 import useSearch from "../../hooks/useSearch";
-import Logo from "../../public/assets/maibo.png";
+import Logo from "../../public/assets/MAIBO.PNG";
 // import LogoIcon from "../../public/assets/maiboIcon.png";
 import {
   addFromLocalStorage,
