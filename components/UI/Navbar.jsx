f497ed6e15e309f475ea907e2808922fcce93b08
TeeJayGTAN <oniretemisan@gmail.com>
2023-11-25 13:35:41 +0100

    fix 8

--- components/UI/Navbar.jsx
+++ components/UI/Navbar.jsx
@@ -11,7 +11,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { db } from "../../firebase";
 import useSearch from "../../hooks/useSearch";
-import Logo from "../../public/assets/maibo.PNG";
+import Logo from "../../public/assets/maibo.png";
 // import LogoIcon from "../../public/assets/maiboIcon.png";
 import {
   addFromLocalStorage,
