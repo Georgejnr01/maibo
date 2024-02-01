b5a4c8ac713c35d37ef53c8e82688ed84aaef7bc
Brad <klausmickelson52@gmail.com>
2024-02-01 19:34:48 +0100

    add sanity folder

---
+++ sanity-studio/schemas/ExchangeRate.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  name: "rate",
+  type: "document",
+  title: "Exchange Rate",
+  fields: [
+    {
+      name: "currency",
+      type: "string",
+      title: "Currency (code)",
+    },
+    {
+      name: "rate",
+      type: "number",
+      title: "Rate",
+      description: "Exchange rate from - to usd",
+    },
+  ],
+};
---
+++ sanity-studio/schemas/index.js
@@ -0,0 +1,6 @@
+// schemas/index.js
+
+import ExchangeRate from "./ExchangeRate";
+import Product from "./Product";
+
+export const schemaTypes = [ExchangeRate, Product];
